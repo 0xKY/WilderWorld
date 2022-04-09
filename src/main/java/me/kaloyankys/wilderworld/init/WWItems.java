e8dda2377c850dd580c1d1fb4115814a69a436b4
KaloyanKKS <k.yunchov9@gmail.com>
2022-04-09 18:44:46 +0100

    2.0.0 Icecream recipes + Tambura behaviour

--- src/main/java/me/kaloyankys/wilderworld/init/WWItems.java
+++ src/main/java/me/kaloyankys/wilderworld/init/WWItems.java
@@ -2,11 +2,13 @@ package me.kaloyankys.wilderworld.init;
 
 import me.kaloyankys.wilderworld.Wilderworld;
 import me.kaloyankys.wilderworld.item.IceCreamItem;
+import me.kaloyankys.wilderworld.item.MusicalItem;
 import me.kaloyankys.wilderworld.util.enums.Flavours;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SpawnEggItem;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -40,6 +42,12 @@ public class WWItems {
     public static final Item ICE_CUBE = register("ice_cube", new Item(new Item.Settings().group(Wilderworld.ICY_ADDITIONS)));
 
     public static final Item MINT = register("mint", new Item(new Item.Settings().group(Wilderworld.ICY_ADDITIONS)));
+
+    public static final Item TAMBURA = register("tambura", new MusicalItem(new Item.Settings().group(Wilderworld.ICY_ADDITIONS),
+            0, SoundEvents.BLOCK_NOTE_BLOCK_GUITAR));
+
+    public static final Item TAMBURA_EBONY = register("tambura_ebony", new MusicalItem(new Item.Settings().group(Wilderworld.ICY_ADDITIONS),
+            0, SoundEvents.BLOCK_NOTE_BLOCK_BASS));
 
     private static Item register(String id, Item item) {
         return Registry.register(Registry.ITEM, new Identifier("wilderworld", id), item);
---
+++ src/main/java/me/kaloyankys/wilderworld/item/MusicalItem.java
@@ -0,0 +1,51 @@
+package me.kaloyankys.wilderworld.item;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class MusicalItem extends Item {
+    private final int trackLength;
+    private final SoundEvent instrument;
+
+    public MusicalItem(Settings settings, int trackLength, SoundEvent instrument) {
+        super(settings);
+        this.trackLength = trackLength;
+        this.instrument = instrument;
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        boolean sneaking = user.isSneaking();
+        float playerPitch = user.getPitch();
+        if (trackLength < 10) {
+            int root = user.getInventory().selectedSlot;
+            int third = root + 4;
+            int fifth = third + 3;
+            if (!sneaking) {
+                world.playSound(user, user.getBlockPos(), instrument, SoundCategory.PLAYERS, 8.0f,
+                        (float) Math.pow(2.0, (root - 12) / 12.0));
+            } else {
+                if (playerPitch == 90) {
+                    third -= 1;
+                } else if (playerPitch == -90) {
+                    fifth += 1;
+                }
+                world.playSound(user, user.getBlockPos(), instrument, SoundCategory.PLAYERS, 8.0f,
+                        (float) Math.pow(2.0, (root - 12) / 12.0));
+                world.playSound(user, user.getBlockPos(), instrument, SoundCategory.PLAYERS, 8.0f,
+                        (float) Math.pow(2.0, (third - 12) / 12.0));
+                world.playSound(user, user.getBlockPos(), instrument, SoundCategory.PLAYERS, 8.0f,
+                        (float) Math.pow(2.0, (fifth - 12) / 12.0));
+            }
+        } else {
+            world.playSound(user, user.getBlockPos(), instrument, SoundCategory.PLAYERS, 4.0f, 1.0f);
+        }
+        return super.use(world, user, hand);
+    }
+}
